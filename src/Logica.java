f8e2cfeb2b743fb6a22a1d7a5a727e9bd528264b
estudiante <estudiante@192.168.115.25>
2017-03-09 09:58:00 -0500

    segundo commit

--- src/Logica.java
+++ src/Logica.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public class Logica {
 	
 	private PApplet app;
 	private String[] nombreIni,datosIni,color;
+	private ArrayList<Palabra> palabras;
 	
 	public Logica(PApplet app){
 		this.app = app;
-		nombreIni = app.loadStrings("usuarios.txt");
+		nombreIni = app.loadStrings("../data/usuarios.txt");
 		datosIni = app.loadStrings("informacion.txt");
 		color = app.loadStrings("tonos.txt");
 	}
@@ -18,7 +21,7 @@ public class Logica {
 			String[] datosDiv = datosIni[i].split("/");
 			String[] colorDiv = color[i].split("/");
 			String nombre = nombreIni[0];
-			String apellio = nombreIni[1];
+			String apellido = nombreIni[1];
 			String cedula = datosIni[0];
 			int edad = Integer.parseInt(datosIni[1]);
 			int peso = Integer.parseInt(datosIni[2]);
@@ -26,6 +29,7 @@ public class Logica {
 			int g = Integer.parseInt(color[1]);
 			int b = Integer.parseInt(color[2]);
 
+			palabras.add(new Palabra(app,nombre,apellido,cedula,edad,peso,r,g,b));
 		}
 	}
 	
